5a81de0895db9fdfeb907c9ac0ffa888982af513
mrak-gg <markiszka@gmail.com>
2023-12-18 23:36:05 +0100

    csomagvaltas mobil nezet, premium megvasarlas 'popup'

--- js/csomagvaltasScript.js
+++ js/csomagvaltasScript.js
@@ -1,3 +1,4 @@
+//desktop dolgok
 //simple session gombok
 var ss_simpleGomb = document.getElementById('ss_simpleGomb');
 var ss_premiumGomb = document.getElementById('ss_premiumGomb');
@@ -5,6 +6,7 @@ var ss_premiumGomb = document.getElementById('ss_premiumGomb');
 var ps_simpleGomb = document.getElementById('ps_simpleGomb');
 var ps_premiumGomb = document.getElementById('ps_premiumGomb');
 
+//session kezeles
 var simpleSession = document.querySelector('.simpleSession');
 var premiumSession = document.querySelector('.premiumSession');
 
@@ -16,4 +18,27 @@ ss_premiumGomb.addEventListener('click', () => {
 ps_simpleGomb.addEventListener('click', () => {
     premiumSession.style.display = 'none';
     simpleSession.style.display = 'flex';
+});
+
+//mobil dolgok
+//simple session gombok
+var ss_simpleGomb_mobil = document.getElementById('ss_simpleGomb_mobil');
+var ss_premiumGomb_mobil = document.getElementById('ss_premiumGomb_mobil');
+
+//premium session
+var ps_simpleGomb_mobil = document.getElementById('ps_simpleGomb_mobil');
+var ps_premiumGomb_mobil = document.getElementById('ps_premiumGomb_mobil');
+
+//session kezeles
+var simpleSession_mobil = document.querySelector('.mobilSimple');
+var premiumSession_mobil = document.querySelector('.mobilPremium');
+
+ss_premiumGomb_mobil.addEventListener('click', () => { 
+    premiumSession_mobil.style.display = 'flex';
+    simpleSession_mobil.style.display = 'none';
+});
+
+ps_simpleGomb_mobil.addEventListener('click', () => {
+    premiumSession_mobil.style.display = 'none';
+    simpleSession_mobil.style.display = 'flex';
 });
