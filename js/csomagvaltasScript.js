266da56ffec35d7931e0738416fb7e605b5ed180
mrak-gg <markiszka@gmail.com>
2023-12-19 22:37:45 +0100

    szilagyi scriptjenek beepitese, kis hibak javitasa

--- js/csomagvaltasScript.js
+++ js/csomagvaltasScript.js
@@ -42,3 +42,52 @@ ps_simpleGomb_mobil.addEventListener('click', () => {
     premiumSession_mobil.style.display = 'none';
     simpleSession_mobil.style.display = 'flex';
 });
+
+//navigacio kod
+const felugroablak = document.getElementById("felugroablak");
+
+function  mobilnavigacio() {
+    
+    console.log("meghivva");
+
+    if (felugroablak.style.display === "none" || felugroablak.style.display === "") 
+    {
+        felugroablak.style.display = "flex";
+    } 
+    else
+    {
+        felugroablak.style.display = "none";
+    }
+}
+
+document.addEventListener('click', function (event) {
+    var target = event.target;
+
+    if (target.id === 'hirdetes_modositasa') {
+        
+        window.location.href = 'hirdetes_megadasa.html';
+    }
+
+
+    if (target.classList.contains('torles')) {
+
+        var torles_wrapper = document.getElementById('torles_wrapper');
+
+        if (torles_wrapper) 
+        {
+            torles_wrapper.style.display = (torles_wrapper.style.display === 'flex') ? 'none' : 'flex';
+        }
+    }
+
+    if(target.id === 'hirdetes_kiemelese')
+    {
+        var item = target.closest('.item');
+
+        var kiemelesBlock = item.querySelector('#kiemelve_block');
+
+        if (kiemelesBlock) {
+            kiemelesBlock.style.display = (kiemelesBlock.style.display === 'flex') ? 'none' : 'flex';
+        }
+
+    }
+});
--- js/fooldalScript.js
+++ js/fooldalScript.js
@@ -11,7 +11,13 @@ hirdeteseimSzovegElement.textContent = 'Hirdetéseim (' + hirdetesekSzama + '/5)
 felhasznalokSzovegElement.textContent = felhasznalokSzama + ' kontakt';
 document.getElementById('kontaktok2').textContent = felhasznalokSzama + ' kontakt';
 
+//navigacios kod
+const felugroablak = document.getElementById("felugroablak");
+
 function  mobilnavigacio() {
+    
+    console.log("meghivva");
+
     if (felugroablak.style.display === "none" || felugroablak.style.display === "") 
     {
         felugroablak.style.display = "flex";
@@ -21,3 +27,35 @@ function  mobilnavigacio() {
         felugroablak.style.display = "none";
     }
 }
+
+document.addEventListener('click', function (event) {
+    var target = event.target;
+
+    if (target.id === 'hirdetes_modositasa') {
+        
+        window.location.href = 'hirdetes_megadasa.html';
+    }
+
+
+    if (target.classList.contains('torles')) {
+
+        var torles_wrapper = document.getElementById('torles_wrapper');
+
+        if (torles_wrapper) 
+        {
+            torles_wrapper.style.display = (torles_wrapper.style.display === 'flex') ? 'none' : 'flex';
+        }
+    }
+
+    if(target.id === 'hirdetes_kiemelese')
+    {
+        var item = target.closest('.item');
+
+        var kiemelesBlock = item.querySelector('#kiemelve_block');
+
+        if (kiemelesBlock) {
+            kiemelesBlock.style.display = (kiemelesBlock.style.display === 'flex') ? 'none' : 'flex';
+        }
+
+    }
+});
