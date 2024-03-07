b0e9a7e9ed677c6db74afba5bfe67fc9a429650b
Mihaly Adam <adammihaly05@gmail.com>
2024-03-07 18:10:30 +0100

    SEO hibak javitva

--- hirdetesek.php
+++ hirdetesek.php
@@ -24,10 +24,10 @@ A gyorsabb és hatékonyabb eladás érdekében a kiemelt hirdetést is használ
 amely még hatékonyabban segíti az eladásban!">
 
 
-	    <meta name="keywords" content="lakasetterem, lakásétterem, étterem, étkezés loadeat, loadeatcom, kaja, ennivaló, menü, etterem, soklakasetterem, lakáséttermek, éttermek, kereső, hírdető, hirdetes, etkeztetes, etteremtulaj, vendég, vendeg, ügyfél, ugyfel, asztalfoglalas, asztalfoglalás">
+	    <meta name="keywords" content="elado allat, állatok, állat, piac, elahely, vétel, vásárlás, háziállat, haszonállat, kutya, macska, eladó, mindenallat.hu">
 
 	    <meta name="author" content="mindenallat.hu">
-	    <link rel="canonical" href="https://mindenallat.com">
+	    <link rel="canonical" href="https://mindenallat.hu">
 
 
 	    <meta property="og:title" content="mindenallat.hu" />
